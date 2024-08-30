a44204a9afe96911877d44d920e41f4f17a2dc11
naveen <naveenari117@gmail.com>
2024-08-30 14:41:50 +0530

    del

--- backup.sh
+++ backup.sh
@@ -18,4 +18,9 @@ fi
 file=$(find $source -name "*.log")
 echo $file
 zipfile=/home/ec2-user/app-2/nav.zip
-find $source -name "*.log" |zip zipfile -@
+find $source -name "*.log" |zip "$zipfile" -@
+
+
+#zipfile1="$destination/nav-logs.zip"
+
+#find ${source} -name "*.log" | zip "$zipfile1" -@
