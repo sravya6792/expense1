39151c4f8c059c7de473d235b47a0807ec50d240
naveen <naveenari117@gmail.com>
2024-08-30 12:36:19 +0530

    backup

--- naveen1.sh
+++ naveen1.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 source=$1
 destination=$2
-if[ $# -ne 2 ]
+if [ $# -ne 2 ]
 then
     echo "plz give arguments"
 fi    
-if [ -d $source ]
-then
-   echo "$source exists"
-else
-    echo "$source not exists"
-fi
-        
+
+    
